a66ee681104d8294ba14815c68eb42ff171e811d
djudju12 <jonathan.willian321@gmail.com>
2023-06-14 19:51:40 -0300

    swagger funcionando

--- src/main/java/com/comidaderuadev/api/ApiApplication.java
+++ src/main/java/com/comidaderuadev/api/ApiApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/api")})
 @SpringBootApplication
 public class ApiApplication {
 
